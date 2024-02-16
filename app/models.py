8d0a590e8283833bbceb19e730dd21717b0b7b33
Mohan <mohan@ramailo.tech>
2024-02-16 16:24:52 +0545

    add models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class UserProfile(AbstractUser):
+    bio = models.TextField(null=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+
+
+class Post(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    content = models.TextField(null=True)
+    image = models.ImageField(upload_to='post_images/', null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
